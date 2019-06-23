d0a7bdb573752a956f6168ecb9f311821d8ba66d
Roderick Buenviaje <rod.buenviaje@gmail.com>
2019-06-23 14:27:57 -0700

    Use bcrypt's has_secure_password functionality

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,16 +1,11 @@
 class User < ApplicationRecord
+  has_secure_password
+
   enum locale: { en: 0 }
 
   has_many :subcriptions
 
   validates_presence_of :username, :email
+  validates_uniqueness_of :username, :email
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-
-  attr_accessor :password
-
-  before_save :encrypt_password
-
-  def encrypt_password
-    # Encrypt password before saving
-  end
 end
--- db/migrate/20190622221439_create_users.rb
+++ db/migrate/20190622221439_create_users.rb
@@ -3,8 +3,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
     create_table :users do |t|
       t.string :uuid, limit: 36, null: false, index: { unique: true }
       t.string :username, null: false
-      t.string :crypted_password
-      t.string :salt
+      t.string :password_digest
       t.string :email, null: false
 
       t.string :first_name
